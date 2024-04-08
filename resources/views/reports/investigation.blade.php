8e595c6d90829b1605ee7c1bbe8feba29cb34edd
Jeremy Siguenza <jersiguenza@my.cspc.edu.ph>
2024-04-08 14:02:46 +0800

    update view

--- resources/views/reports/investigation.blade.php
+++ resources/views/reports/investigation.blade.php
@@ -98,73 +98,38 @@
                 <div class="modal-dialog modal-dialog-centered modal-xl">
                     <div class="modal-content">
                         <div class="modal-header">
+                            <h5 class="modal-title">Reports</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body m-3">
-                            <h3 class="mb-2">Reports</h3>
-                            <div class="d-flex justify-content-end mb-3">
-                                <span class="btn btn-primary w-10">Print</span>
-                            </div>
-                            <div class="shadow rounded p-4">
-                                <table class="table w-100 text-center">
-                                    <thead class="text-dark fs-4">
-                                        <tr>
-                                            <th class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-0">Name</h6>
-                                            </th>
-                                            <th class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-0">Team Leader</h6>
-                                            </th>
-                                            <th class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-0">Type</h6>
-                                            </th>
-                                            <th class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-0">Driver</h6>
-                                            </th>
-                                            <th class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-0">Departure From Station</h6>
-                                            </th>
-                                            <th class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-0">Arrival to Station</h6>
-                                            </th>
-                                            
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                        <div class="modal-body p-4">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col">
                                         @foreach ($investigation as $report)
-                                            <tr>
-                                                <td class="border-bottom-0">
-                                                    <h6 class="fw-semibold mb-0">{{ $report->name }}</h6>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <h6 class="fw-semibold mb-0 text-capitalize">
-                                                        {{ $report->personRank($report->teamLeader->ranks_id)->slug . ' ' . $report->teamLeader->last_name }}
-                                                    </h6>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <p class="mb-0 fw-normal">{{ $report->type }}</p>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <p class="mb-0 fw-normal text-capitalize">
-                                                        {{ $report->personRank($report->driver->ranks_id)->slug . ' ' . $report->driver->last_name }}
-                                                    </p>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <p class="mb-0 fw-normal">{{ $report->time_of_departure }}</p>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <p class="mb-0 fw-normal">{{ $report->time_of_arrival_to_station }}</p>
-                                                </td>
-                                               
-                                            </tr>
+                                        <div class="card mb-4">
+                                            <div class="card-body">
+                                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                                    <h4 class="text-primary">{{ $report->name }}</h4>
+                                                    <span class="btn btn-primary w-10 d-flex">Print</span>
+                                                </div>
+                                                <ul class="list-group list-group-flush">
+                                                    <li class="list-group-item"><strong>Team Leader:</strong> {{ $report->personRank($report->teamLeader->ranks_id)->slug . ' ' . $report->teamLeader->last_name }}</li>
+                                                    <li class="list-group-item"><strong>Type:</strong> {{ $report->type }}</li>
+                                                    <li class="list-group-item"><strong>Driver:</strong> {{ $report->personRank($report->driver->ranks_id)->slug . ' ' . $report->driver->last_name }}</li>
+                                                    <li class="list-group-item"><strong>Departure From Station:</strong> {{ $report->time_of_departure }}</li>
+                                                    <li class="list-group-item"><strong>Arrival to Station:</strong> {{ $report->time_of_arrival_to_station }}</li>
+                                                </ul>
+                                            </div>
+                                        </div>
                                         @endforeach
-                                    </tbody>
-                                </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div> 
+            </div>
+            
 
             <!-- Update Investigation  -->
             <div class="modal fade" data-bs-backdrop="static" id="updateModal{{ $report->id }}" tabindex="-1" aria-labelledby="updateModalLabel{{ $report->id }}" aria-hidden="true">
