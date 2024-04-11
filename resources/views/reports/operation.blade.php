e49938dd28e24c52b5c69c9d44fabddfd1642531
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-04-11 08:26:50 +0800

    view and update modal

--- resources/views/reports/operation.blade.php
+++ resources/views/reports/operation.blade.php
@@ -51,6 +51,7 @@
                                     <tbody>
                                         @foreach ($reports as $report)
                                         <x-reports.view-modal :report=$report></x-reports.view-modal>
+                                        <x-reports.update :report=$report></x-reports.update>
                                             <tr>
                                                 <td class="border-bottom-0">
                                                     <h6 class="fw-semibold mb-0">{{ $report->name }}</h6>
@@ -89,92 +90,9 @@
 
              
         
-            <!-- Update Operation  -->
-            <div class="modal fade" data-bs-backdrop="static" id="updateModal{{ $report->id }}" tabindex="-1" aria-labelledby="updateModalLabel{{ $report->id }}" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="updateModalLabel{{ $report->id }}">Update Report</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
-                                            <label for="name" class="form-label">Name</label>
-                                            <input type="text" class="form-control" id="name" name="name" value="{{ $report->name }}">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
-                                            <label for="team_leader" class="form-label">Team Leader</label>
-                                            <select class="form-select" id="team_leader" name="team_leader">
-                                                <option value="{{ $report->teamLeader->id }}">{{ $report->personRank($report->teamLeader->ranks_id)->slug . ' ' . $report->teamLeader->last_name }}</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
-                                            <label for="type" class="form-label">Type</label>
-                                            <select class="form-select" id="type" name="type">
-                                                <option value="{{ $report->type }}">{{ $report->type }}</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
-                                            <label for="driver" class="form-label">Driver</label>
-                                            <select class="form-select" id="driver" name="driver">
-                                                <option value="">driver</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
-                                            <label for="departure_time" class="form-label">Departure Time</label>
-                                            <input type="datetime-local" class="form-control" id="departure_time" name="departure_time" value="{{ $report->time_of_departure }}">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
-                                            <label for="arrival_time" class="form-label">Arrival Time</label>
-                                            <input type="datetime-local" class="form-control" id="arrival_time" name="arrival_time" value="{{ $report->time_of_arrival_to_station }}">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="submit" class="btn btn-success">Update</button>
-                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
             
-            <!-- Delete Confirmation Modal -->
-            <div class="modal fade" data-bs-backdrop="static" id="deleteModal{{ $report->id }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $report->id }}" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered text-center">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title" id="deleteModalLabel">Confirm Deletion</h4>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                           <h5>Are you sure you want to delete this report?</h5> 
-                        </div>
-                        <div class="modal-footer d-flex justify-content-around">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Yes</button>
-                            <button type="button" id="confirmDeleteBtn" class="btn btn-danger" data-bs-dismiss="modal">No</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
+            
 
             <x-reports.chooseReport :reports=$investigation :category="'Investigation'"> </x-reports.chooseReport>
             <x-reports.chosen :category=$active > </x-reports.chosen>
