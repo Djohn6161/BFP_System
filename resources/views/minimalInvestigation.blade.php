b0eba95b6f3aaaa3cd6b74f431a5082e34c8954a
Jeremy Siguenza <jersiguenza@my.cspc.edu.ph>
2024-04-19 10:52:42 +0800

    fix minimal report

--- resources/views/minimalInvestigation.blade.php
+++ resources/views/minimalInvestigation.blade.php
@@ -37,6 +37,38 @@
                     <form>
                         <div class="row border border-light-subtle shadow rounded p-4 mb-4">
                             {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Fire Incident Response Details</h3> --}}
+                            <h3 class="border-bottom border-4 border-warning pb-2 mb-3">MEMORANDUM</h3>
+                            {{-- <h5>Details</h5> --}}
+                            <div class="col-lg-12 mb-12 pb-2 mb-3">
+                                <label for="dateTime" class="form-label">FOR:</label>
+                                <input type="text" placeholder="Eg. pedro villa" class="form-control text-uppercase" id="dateTime" required>
+                            </div>
+ 
+                            <div class="col-lg-12 mb-12 pb-2 mb-3">
+                                <label for="reported" class="form-label">SUBJECT:</label>
+                                <input type="text" placeholder="Eg. fire incident report " class="form-control text-uppercase" id="reported" required>
+
+                            </div>
+                            <div class="col-lg-12 mb-12 pb-2 mb-3">
+                                <label for="province" class="form-label">DATE:</label>
+                                <input type="text" placeholder=" Eg. march 02, 2013" class="form-control" id="province" required>
+                            </div>
+                        </div>
+
+                        <div class="row border border-light-subtle shadow rounded p-4 mb-4">
+                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Fire Incident Response Details</h3> --}}
+                            <h3 class="border-bottom border-4 border-warning pb-2 mb-3"></h3>
+                            {{-- <h5>Details</h5> --}}
+                            <div class="col-lg-12 mb-12 pb-5 mb-3">
+                                <label for="dateTime" class="form-label"></label>
+                                <div>
+                                    <div id="editor"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row border border-light-subtle shadow rounded p-4 mb-4">
+                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Fire Incident Response Details</h3> --}}
                             <h3 class="border-bottom border-4 border-warning pb-2 mb-3">DETAILS</h3>
                             {{-- <h5>Details</h5> --}}
                             <div class="col-lg-6 mb-3">
@@ -189,4 +221,14 @@ input.addEventListener('input', function() {
     }
 });
     </script>
+
+<script>
+    const quill = new Quill('#editor', {
+      theme: 'snow',
+
+      placeholder: 'Compose an epic...',
+    });
+
+    
+  </script>
 @endsection
