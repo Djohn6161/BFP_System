35033b3b94171b17bab94f0cb60c7235a6b9805e
Benedict Reyes <hanzbndctreyes18@gmail.com>
2024-04-22 16:41:57 +0800

    view investigation

--- resources/views/components/reports/view-investigation.blade.php
+++ resources/views/components/reports/view-investigation.blade.php
@@ -1,32 +1,14 @@
-    <div class="modal fade" id="chooseInvestigation" tabindex="-1" aria-labelledby="noModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h3 class="modal-title" id="noModalLabel">Choose chuchuchu</h3>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <a href="#" class="btn btn-lg btn-outline-primary d-block w-100 mb-2">Minimal</a>
-                    <a href="#" class="btn btn-lg btn-outline-primary d-block w-100 mb-2">Spot</a>
-                    <button type="button" class="btn btn-lg btn-outline-primary d-block w-100 mb-2" onclick="progress(this)">Progress</button>
-                    <button type="button" class="btn btn-lg btn-outline-primary d-block w-100 mb-2" onclick="progress(this)">Final</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Second Modal (Yes Modal) -->
-<div class="modal fade" id="spotTable" tabindex="-1" aria-labelledby="yesModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl">
+<div class="modal modal-xl fade" id="viewInvestigation" tabindex="-1" aria-labelledby="noModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h3 class="modal-title" id="noModalLabel">View Details</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
             </div>
-            <div class="modal-body m-3 text-center">
-                <h3 class="mb-2">Spot Reports</h3>
+            <div class="modal-body">
                 <div class="shadow rounded p-4">
-                    <table class="table w-100 " id="spotJTable">
+                    <table class="table table-striped">
                         <thead class="text-dark fs-4">
                             <tr>
                                 <th class="border-bottom-0">
@@ -83,4 +65,3 @@
         </div>
     </div>
 </div>
-
--- resources/views/reports/investigation.blade.php
+++ resources/views/reports/investigation.blade.php
@@ -16,14 +16,12 @@
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#chooseInvestigation">Create</button>
                     <x-reports.create-investigation></x-reports.create-investigation>
                 </div>
-                {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Launch demo modal
-                  </button> --}}
                 <div class="col-lg-12 d-flex align-items-stretch">
                     <div class="card w-100">
                         <div class="card-body p-4">
                             <h5 class="card-title fw-semibold mb-4">Investigation Minimal Reports</h5>
                             <div class="table-responsive">
+                                <x-reports.view-investigation></x-reports.view-investigation>
                                 <table class="table mb-0 align-middle w-100">
                                     <thead class="text-dark fs-4">
                                         <tr>
@@ -42,8 +40,9 @@
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        
                                         @foreach ($investigations as $investigation)
-                                            <x-reports.view-modal :report=$investigation></x-reports.view-modal>
+                                            
                                             {{-- <x-reports.update :report=$investigation></x-reports.update> --}}
                                             <tr>
                                                 <td class="border-bottom-0">
@@ -56,6 +55,8 @@
                                                     <p class="mb-0 fw-normal">{{ \Carbon\Carbon::parse($investigation->date)->format('F j, Y') }}</p>
                                                 </td>
                                                 <td class="border-bottom-0">
+                                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#viewInvestigation">View</button>
+                                                    {{-- <x-reports.view-investigation></x-reports.view-investigation> --}}
                                                     <a href="{{route('operation.edit.form', $investigation->id)}}" class="btn btn-success w-100 mb-1">Update</a>
                                                     <br>
                                                     <a href="#" data-bs-toggle="modal"
