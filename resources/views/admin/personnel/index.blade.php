1d3fefcf5e833306f9127e8d7aaa725e2cfab2db
Craig Ben Cadag <craigbencadag@gmail.com>
2024-04-30 10:24:50 +0800

    personnel update

--- resources/views/admin/personnel/index.blade.php
+++ resources/views/admin/personnel/index.blade.php
@@ -13,7 +13,7 @@
                         </span>
                         <span class="hide-menu">Add Personnel</span>
                     </button>
-                    
+
                 </div>
                 <div class="col-lg-12">
                     <div class="card w-100">
@@ -43,12 +43,56 @@
 
                             <!-- Display Total Personnel -->
                             <h5 class="fw-semibold mb-4">BFP - Personnel
-                                <span class=" ms-3 badge rounded-pill bg-secondary">{{ count($personnelData) }}</span>
+                                <span class=" ms-3 badge rounded-pill bg-secondary">{{ $personnelCount }}</span>
                             </h5>
 
                             <!-- Accordion for Ranks -->
                             <div class="accordion accordion-flush" id="accordionRankPersonnel">
-                                @foreach ($personnelCountByRank as $rank => $count)
+                                @foreach ($ranks as $rank)
+                                    <?php $count = 0; ?>
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header">
+                                            <button class="accordion-button collapsed" type="button"
+                                                data-bs-toggle="collapse"
+                                                data-bs-target="#flush-collapse{{ $loop->index + 1 }}" aria-expanded="false"
+                                                aria-controls="flush-collapse{{ $loop->index + 1 }}">
+                                                {{ $rank->slug }} - {{ $rank->name }}
+                                                @foreach ($personnels as $personnel)
+                                                    @if ($personnel->ranks_id == $rank->id)
+                                                        <?php $count++; ?>
+                                                    @endif
+                                                @endforeach
+                                                <span
+                                                    class="ms-3 badge rounded-pill bg-secondary">{{ $count }}</span>
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapse{{ $loop->index + 1 }}" class="accordion-collapse collapse"
+                                            data-bs-parent="#accordionRankPersonnel">
+                                            <div class="row row-cols-1 row-cols-md-4 g-4 p-3 mt-2">
+                                                @foreach ($personnels as $personnel)
+                                                    @if ($personnel->ranks_id == $rank->id)
+                                                        <div class="card justify-content-center m-2"
+                                                            style="width: calc(25% - 1rem); p-1;">
+                                                            <a href="{{ route('personnel.view', $personnel->id) }}">
+                                                                <div class="col py-2">
+                                                                    <!-- Display Personnel Details -->
+                                                                    <img src="{{ asset('assets/images/backgrounds/sir sample.jpg') }}"
+                                                                        class="card-img-top object-fit-cover rounded"
+                                                                        height="300" alt="personnel picture">
+                                                                    <div class="card-body">
+                                                                        <h5 class="card-title text-center fw-bold fs-5">
+                                                                            {{ $personnel->first_name }} {{$personnel->last_name}}</h5>
+                                                                    </div>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                                {{-- @foreach ($personnelCountByRank as $rank => $count)
                                     <div class="accordion-item">
                                         <h2 class="accordion-header">
                                             <button class="accordion-button collapsed" type="button"
@@ -85,7 +129,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                @endforeach
+                                @endforeach --}}
                             </div>
                         </div>
                     </div>
@@ -94,7 +138,7 @@
         </div>
     </div>
 
-    
+
     <x-personnel.add :category="$active"> </x-personnel.add>
     {{-- <x-personnel.preview :category="$active"> </x-personnel.preview> --}}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InvestigationController;
 use App\Http\Controllers\OperationController;
+use App\Http\Controllers\PersonnelController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UsersController;
@@ -81,7 +82,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('/personnel/create', [AdminController::class, 'createPersonnel'])->name('personnel.create');
         Route::get('/personnel/view', [AdminController::class, 'reviewPersonnel'])->name('personnel.view');
         Route::get('/personnel/edit', [AdminController::class, 'editPersonnel'])->name('personnel.edit');
-        
 
         // Account
         Route::get('/account', [AdminController::class, 'accountIndex'])->name('account');
@@ -91,18 +91,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('/account/password/update', [AdminController::class, 'accountPasswordUpdate'])->name('account.password.update');
     });
 
-    // Reports
-
-
-    // Route::get('/report/create/{id}/{type}/{category}', [ReportController::class, 'createReport'])->name('report.create');
-    // Route::post('/report/store/{category}', [ReportController::class, 'storeReport'])->name('report.store');
-
-    // Afor
-    Route::get('/reports/Operation/index', [OperationController::class, 'operationIndex'])->name('operation.index');
-    Route::get('/reports/operation/create/form', [OperationController::class, 'operationCreateForm'])->name('operation.create.form');
-    Route::post('/reports/operation/create/submit', [OperationController::class, 'operationStore'])->name('operation.create');
-    Route::get('/reports/operation/update/form/{id}', [OperationController::class, 'operationUpdateForm'])->name('operation.update.form');
-    Route::post('/reports/operation/update/submit', [OperationController::class, 'operationUpdate'])->name('operation.update');
+    // Operation
+    Route::prefix('reports/operation')->name('operation.')->group(function () {
+        Route::get('/index', [OperationController::class, 'operationIndex'])->name('index');
+        Route::get('/create/form', [OperationController::class, 'operationCreateForm'])->name('create.form');
+        Route::post('/create/submit', [OperationController::class, 'operationStore'])->name('create');
+        Route::get('/update/form/{id}', [OperationController::class, 'operationUpdateForm'])->name('update.form');
+        Route::post('/update/submit', [OperationController::class, 'operationUpdate'])->name('update');
+    });
 
     // Investigation
     Route::prefix('reports/investigation')->name('investigation.')->group(function () {
@@ -125,6 +121,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 
         // Route::get('/create/form', [InvestigationController::class, 'investigationMinimalCreateForm'])->name('minimal.create.form');
+    });
+
+    // Personnel    
+    Route::prefix('personnel/')->name('personnel.')->group(function () {
+        Route::get('/index', [PersonnelController::class, 'personnelIndex'])->name('index');
+        Route::get('/update/{id}', [PersonnelController::class, 'personnelView'])->name('view');
+        // Route::post('/create/submit', [OperationController::class, 'operationStore'])->name('create');
+        // Route::get('/update/form/{id}', [OperationController::class, 'operationUpdateForm'])->name('update.form');
+        // Route::post('/update/submit', [OperationController::class, 'operationUpdate'])->name('update');
     });
 
 
