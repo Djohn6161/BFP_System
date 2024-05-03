82e191a8f1c6b3dbca7347db17f4dcbf9d707caa
Nesjohn Joelle M. Felices <nesjohnfelices@gmail.com>
2024-05-03 13:02:28 +0800

    update image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
             <div class="card mb-0">
               <div class="card-body">
                 <a href="" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="../assets/images/logos/bfp.gif" width="150" alt="">
+                  <img src="../assets/images/logos/logo3.png" width="170" alt="">
                 </a>
                 <p class="text-center"><b>Bureau of Fire Protection<br> Ligao City<b></p>
                 
