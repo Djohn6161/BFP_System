6caba3603772b686aa5d57a930ccf71c944b7530
Nesjohn Joelle M. Felices <nesjohnfelices@gmail.com>
2024-05-03 11:11:13 +0800

    update login page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,20 +13,20 @@
 
 </head>
 
-<body style="background-image: url('../assets/images/backgrounds/b.jpg'); background-size: cover; background-position: center;">
+<body style="background-image: url('../assets/images/backgrounds/bg1.png'); background-size: cover; background-position: center;">
   <!--  Body Wrapper -->
   <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
     data-sidebar-position="fixed" data-header-position="fixed">
-    <div class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+    <div class="position-relative overflow-hidden  min-vh-100 d-flex align-items-center justify-content-center">
       <div class="d-flex align-items-center justify-content-center w-100">
         
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
                 <a href="" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="../assets/images/logos/logo.jpg" width="80" alt="">
+                  <img src="../assets/images/logos/bfp.gif" width="150" alt="">
                 </a>
-                <p class="text-center">Bureau of Fire Protection- Ligao City</p>
+                <p class="text-center"><b>Bureau of Fire Protection<br> Ligao City<b></p>
                 
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
