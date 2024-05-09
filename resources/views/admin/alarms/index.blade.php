2f337ad4f68c93680d9756d04abfadbd10424206
Cristine Haber <haber650@gmail.com>
2024-05-09 14:19:54 +0800

    Update Alarms Index

--- resources/views/admin/alarms/index.blade.php
+++ resources/views/admin/alarms/index.blade.php
@@ -25,31 +25,34 @@
                                 <table class="table mb-0 align-middle w-100" id="operationTable">
                                     <thead class="text-dark fs-4 ">
                                         <tr>
-                                            <th class="border-bottom-0 text-center"> Alarm Name</th>
-                                            <th class="border-bottom-0 text-center">Action</th>
+                                            <th class="border-bottom-0"> Alarm Name</th>
+                                            <th class="border-bottom-0">Action</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
+                                    <tbody class="table-group-divider">
                                         @foreach ($alarm_list as $list)
                                             <x-alarm.edit :list="$list"></x-alarm.edit> 
-                                            <tr class="text-center">
+                                            <tr>
                                                 <td class="border-bottom-0">{{ $list->name }}</td>
 
-                                                <td class="border-bottom-0">
-                                                
-                                                    <button href="editAlarmModal" class="btn btn-success w-100 mb-1" data-bs-toggle="modal"
-                                                    data-bs-target="#editAlarmModal{{ $list->id }}">Update
-                                                    
-
-                                                    <i class="ti ti-pencil"></i>
-                                                </button>
-                                                    <br>
-                                                    <button type="button" class="btn btn-danger w-100 mb-1" data-bs-toggle="modal"
-                                                    data-bs-target="#deleteAlarmModalll{{ $list->id }}">Delete
-                                                    <i class="ti ti-trash"></i>
-                                                </button>
-                                                    <x-alarm.delete :list="$list"></x-alarm.delete>
-
+                                                <td class="w-25 py-2">
+                                                    <div class="d-flex flex-row">
+                                                        <div class="me-1">
+                                                            <button class="btn btn-success w-100 mb-1"
+                                                                data-bs-toggle="modal"
+                                                                data-bs-target="#editAlarmModal{{ $list->id }}">
+                                                                Update
+                                                                <i class="ti ti-pencil"></i>
+                                                            </button>
+                                                        </div>
+                                                        <div class="me-1">
+                                                            <button class="btn btn-danger w-100 mb-1" data-bs-toggle="modal"
+                                                                data-bs-target="#deleteAlarmModalll{{ $list->id }}">
+                                                                Delete
+                                                                <i class="ti ti-trash"></i>
+                                                            </button>
+                                                        </div>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         @endforeach
