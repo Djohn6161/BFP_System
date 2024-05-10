ba612c03adcb30200dac18dc81b3e5793a01df48
Craig Ben Cadag <craigbencadag@gmail.com>
2024-05-10 13:33:59 +0800

    update

--- database/migrations/2024_04_12_025157_create_afor_designations.php
+++ database/migrations/2024_04_12_025157_create_afor_designations.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('afor_designations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('afor_id')->nullable()->constrained('afors')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('key');
             $table->string('name');
             $table->timestamps();
         });
