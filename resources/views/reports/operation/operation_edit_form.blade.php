dd6434ba8e6e080dfa407146bbd6b8e777bc60fa
Craig Ben Cadag <craigbencadag@gmail.com>
2024-05-10 09:51:35 +0800

    update

--- resources/views/reports/operation/operation_edit_form.blade.php
+++ resources/views/reports/operation/operation_edit_form.blade.php
@@ -8,8 +8,8 @@
             <div class="col-lg-11 p-4">
                 <div class="row">
                     <form method="POST" action="{{ route('operation.update') }}" enctype="multipart/form-data">
-                        @csrf
-
+                        @csrf 
+                        
                         <!-- Intro -->
                         <div class="row border border-light-subtle shadow rounded p-4 mb-4">
                             <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">1</h3>
