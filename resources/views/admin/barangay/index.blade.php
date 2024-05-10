501e7df43dd649fe64db61c61430eaa782ed3305
Cristine Haber <haber650@gmail.com>
2024-05-10 15:45:59 +0800

    Update Barangay View

--- resources/views/admin/barangay/index.blade.php
+++ resources/views/admin/barangay/index.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-lg-12 d-flex align-items-stretch">
                     <div class="card w-100">
                         <div class="card-body p-4">
-                            <h5 class="card-title fw-semibold mb-4">Barangay</h5>
+                            <h5 class="card-title fw-semibold mb-4 p-3 rounded bg-gradient-blue text-light">Barangay</h5>
                             <div class="table-responsive">
                                 <table class="table mb-0 align-middle w-100" id="barangayTable">
                                     <thead class="text-dark fs-4">
