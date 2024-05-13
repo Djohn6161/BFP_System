6f884c9c33f14892428795d75f4172c9577a5505
Cristine Haber <haber650@gmail.com>
2024-05-13 14:02:43 +0800

    Update Investigation View

--- resources/views/reports/investigation/spot.blade.php
+++ resources/views/reports/investigation/spot.blade.php
@@ -5,11 +5,11 @@
 </style>
 @extends('layouts.user-template')
 @section('content')
-    <div class="container-fluid">
-        <!--  Row 1 -->
+    {{-- <div class="container-fluid">
+       
 
         <div class="col-lg-12">
-            <!-- Monthly Earnings -->
+           
 
             <div class="row">
                 <div class="col d-flex justify-content-end mb-2">
@@ -17,14 +17,30 @@
                         data-bs-target="#chooseInvestigation">Create</button>
                     <x-reports.create-investigation :spots=$spots></x-reports.create-investigation>
                 </div>
-                {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Launch demo modal
-                  </button> --}}
+               
                 <div class="col-lg-12 d-flex align-items-stretch">
                     <div class="card w-100">
                         <div class="card-body p-4">
                             <h5 class="card-title fw-semibold mb-4 text-capitalize">
-                                {{ $active != 'investigation' ? $active : 'All' }} Investigation Reports</h5>
+                                {{ $active != 'investigation' ? $active : 'All' }} Investigation Reports</h5> --}}
+
+    <div class="container-fluid">
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-12 d-flex align-items-stretch">
+                    <div class="card w-100">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between align-items-center p-3 rounded bg-gradient-blue">
+                                <h5 class="mb-0 text-light card-title fw-semibold text-capitalize">
+                                    {{ $active != 'investigation' ? $active : 'All' }} Investigation Reports
+                                </h5>
+                                <button type="button" class="btn btn-light" data-bs-toggle="modal"
+                                    data-bs-target="#chooseInvestigation">
+                                    <i class="ti ti-plus"></i>
+                                    Create
+                                </button>
+                                <x-reports.create-investigation :spots=$spots></x-reports.create-investigation>
+                            </div>
                             <div class="table-responsive">
                                 <table class="table mb-0 align-middle w-100" id="spotInvestigationTable">
                                     <thead class="text-dark fs-4">
@@ -64,16 +80,19 @@
                                                 <td class="border-bottom-0">
                                                     <button type="button" data-bs-toggle="modal"
                                                         data-bs-target="#viewSpotModal{{ $investigation->id }}"
-                                                        class="btn btn-primary hide-menu w-100 mb-1"><i class="ti ti-eye"></i> View</button>
+                                                        class="btn btn-primary hide-menu w-100 mb-1"><i
+                                                            class="ti ti-eye"></i> View</button>
                                                     <x-reports.Investigation.view-spot
                                                         :investigation=$investigation></x-reports.Investigation.view-spot>
 
                                                     <a href="{{ route('investigation.spot.edit', ['spot' => $investigation->id]) }}"
-                                                        class="btn btn-success w-100 mb-1"><i class="ti ti-pencil"></i> Update</a>
+                                                        class="btn btn-success w-100 mb-1"><i class="ti ti-pencil"></i>
+                                                        Update</a>
                                                     <br>
                                                     <button type="button" data-bs-toggle="modal"
                                                         data-bs-target="#deleteModal{{ $investigation->id }}"
-                                                        class="btn btn-danger hide-menu w-100 mb-1"><i class="ti ti-trash"></i> Delete</button>
+                                                        class="btn btn-danger hide-menu w-100 mb-1"><i
+                                                            class="ti ti-trash"></i> Delete</button>
                                                     <x-reports.investigation.investigation-delete :type="'spot'"
                                                         :investigation=$investigation></x-reports.investigation.investigation-delete>
 
--- resources/views/reports/operation/operation.blade.php
+++ resources/views/reports/operation/operation.blade.php
@@ -31,11 +31,10 @@
                         <div class="card-body p-4">
                             <div class="d-flex justify-content-between align-items-center p-3 rounded bg-gradient-blue">
                                 <h5 class="mb-0 text-light card-title fw-semibold">Operation Reports</h5>
-                                <button type="button" class="btn btn-light" href="{{ route('operation.create.form') }}">
+                                <button class="btn btn-light" href="{{ route('operation.create.form') }}">
                                     <i class="ti ti-plus"></i>
                                     Create
                                 </button>
-                                <x-truck.create :category="$active"></x-truck.create>
                             </div>
                             <div class="table-responsive">
                                 <table class="table mb-0 align-middle w-100" id="operationTable">
