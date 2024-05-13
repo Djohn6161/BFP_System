3604b45c632539770c9df8dea468e4d30764e217
Cristine Haber <haber650@gmail.com>
2024-05-13 09:13:46 +0800

    Update View in Trucks

--- resources/views/admin/trucks/index.blade.php
+++ resources/views/admin/trucks/index.blade.php
@@ -5,7 +5,7 @@
 </style>
 @extends('layouts.user-template')
 @section('content')
-    <div class="container-fluid">
+    {{-- <div class="container-fluid">
         <div class="col-lg-12">
             <div class="row">
                 <div class="col d-flex justify-content-end mb-2">
@@ -20,7 +20,24 @@
                         <div class="card-body p-4">
                             <h5 class="card-title fw-semibold mb-4 p-3 rounded bg-gradient-blue text-light">
                                 Station Trucks
-                            </h5>
+                            </h5> --}}
+
+    <div class="container-fluid">
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-12 d-flex align-items-stretch">
+                    <div class="card w-100">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between align-items-center p-3 rounded bg-gradient-blue">
+                                <h5 class="mb-0 text-light card-title fw-semibold">Trucks</h5>
+                                <button type="button" class="btn btn-light" data-bs-toggle="modal"
+                                    data-bs-target="#addTruckModal">
+                                    <i class="ti ti-plus"></i>
+                                    Create
+                                </button>
+                                <x-truck.create :category="$active"></x-truck.create>
+                            </div>
+
                             <div class="table-responsive">
                                 <table class="table mb-0 align-middle w-100">
                                     <thead class="text-dark fs-4">
