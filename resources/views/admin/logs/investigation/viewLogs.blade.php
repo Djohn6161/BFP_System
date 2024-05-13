9e5f29066c76e340da3fd30d381f3373adcd0e6f
Cristine Haber <haber650@gmail.com>
2024-05-13 09:14:19 +0800

    Update Logs

--- resources/views/admin/logs/investigation/viewLogs.blade.php
+++ resources/views/admin/logs/investigation/viewLogs.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.user-template')
 @section('content')
-    <div class="container-fluid">
-
-
-        <div class="col-lg-12">
-
-
-            <div class="row">
-                <div class="col d-flex justify-content-end mb-2">
-                </div>
-                <div class="col-lg-12 d-flex align-items-stretch">
-                    <div class="card w-100">
-                        <div class="card-body p-4">
-                            <h5 class="card-title fw-semibold mb-4">Investigation Logs</h5>
+   
+<div class="container-fluid">
+    <div class="col-lg-12">
+        <div class="row">
+            <div class="col-lg-12 d-flex align-items-stretch">
+                <div class="card w-100">
+                    <div class="card-body p-4">
+                        <div class="d-flex justify-content-between align-items-center p-3 rounded bg-gradient-blue">
+                            <h5 class="mb-0 text-light card-title fw-semibold">Investigation Logs</h5>
+                            
+                        </div>
 
                             <div class="table-responsive">
                                 <table class="table table-hover table-striped">
