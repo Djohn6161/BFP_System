19c203ad72ddcc320fd3b72de133a22d8d408361
Cristine Haber <haber650@gmail.com>
2024-05-13 16:30:55 +0800

    Update View
    Responsive header

--- resources/views/admin/trash/operation/index.blade.php
+++ resources/views/admin/trash/operation/index.blade.php
@@ -5,7 +5,7 @@
 </style>
 @extends('layouts.user-template')
 @section('content')
-    <div class="container-fluid">
+    {{-- <div class="container-fluid">
         <!--  Row 1 -->
 
         <div class="col-lg-12">
@@ -19,7 +19,17 @@
                     <div class="card w-100">
                         <div class="card-body p-4">
                             <h5 class="card-title fw-semibold mb-4 text-capitalize">
-                                Trash {{ $active != 'operation' ? $active : 'All' }}</h5>
+                                Trash {{ $active != 'operation' ? $active : 'All' }}</h5> --}}
+
+    <div class="container-fluid">
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-12 d-flex align-items-stretch">
+                    <div class="card w-100">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between align-items-center p-3 rounded bg-gradient-blue">
+                                <h5 class="mb-0 text-light card-title fw-semibold">Operation Trash</h5>
+                            </div>
                             <div class="table-responsive">
                                 <table class="table mb-0 align-middle w-100" id="trashOperationTable">
                                     <thead class="text-dark fs-4">
