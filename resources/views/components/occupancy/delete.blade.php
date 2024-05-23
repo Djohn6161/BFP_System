31b037b62ac954abe053d645974e323b76725c9f
Cristine Haber <haber650@gmail.com>
2024-05-23 16:51:20 +0800

    Update Delete Modal
    Occupancy

--- resources/views/components/occupancy/delete.blade.php
+++ resources/views/components/occupancy/delete.blade.php
@@ -1,4 +1,4 @@
-<!-- Modal -->
+{{-- <!-- Modal -->
 <div class="modal fade" data-bs-backdrop="static" id="deleteOccupancyModal{{ $occupancyName->id }}" tabindex="-1" aria-labelledby="deleteOccupancyModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -23,6 +23,30 @@
             </div>
             </form>
             
+        </div>
+    </div>
+</div> --}}
+
+<!-- Modal -->
+<div class="modal fade" data-bs-backdrop="static" id="deleteOccupancyModal{{ $occupancyName->id }}" tabindex="-1" aria-labelledby="deleteOccupancyModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <form method="POST" action="{{route('admin.occupancy_name.delete', ['id' =>$occupancyName->id])}}" enctype="multipart/form-data">
+                @csrf
+                @method('DELETE')
+                <div class="modal-body text-center p-0">
+                    <div class="modal-icon mt-3">
+                        <img src="/assets/images/icons/delete.gif" alt="Warning Icon">
+                    </div>
+                    <h4 class="modal-title" id="deleteOccupancyModal">Delete this <strong>"{{ $occupancyName->name }}"</strong> occupancy?</h4>
+                    <p>The data associated with this occupancy will be lost.</p>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-danger">Yes, Delete!</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
