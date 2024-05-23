48e278ef81b891a3729fafbff5ccaea3fd79aa5a
Cristine Haber <haber650@gmail.com>
2024-05-23 16:51:27 +0800

    Update Delete Modal
    Barangay

--- resources/views/components/barangay/delete.blade.php
+++ resources/views/components/barangay/delete.blade.php
@@ -1,4 +1,4 @@
-<!-- Modal -->
+{{-- <!-- Modal -->
 <div class="modal fade" data-bs-backdrop="static" id="deleteBarangayModal{{ $barangay->id }}" tabindex="-1" aria-labelledby="deleteBarangayModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
@@ -15,6 +15,30 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-danger">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div> --}}
+
+
+<!-- Modal -->
+<div class="modal fade" data-bs-backdrop="static" id="deleteBarangayModal{{ $barangay->id }}" tabindex="-1" aria-labelledby="deleteBarangayModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <form method="POST" action="{{ route('admin.barangay.delete', ['id' => $barangay->id]) }}">
+                @csrf
+                @method('DELETE')
+                <div class="modal-body text-center p-0">
+                    <div class="modal-icon mt-3">
+                        <img src="/assets/images/icons/delete.gif" alt="Warning Icon">
+                    </div>
+                    <h4 class="modal-title" id="deleteBarangayModalLabel">Delete this <strong>"{{ $barangay->name }}"</strong> truck?</h4>
+                    <p>The data associated with this baramgay will be lost.</p>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-danger">Yes, Delete!</button>
                 </div>
             </form>
         </div>
