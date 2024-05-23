fabf6dbd9827e3b87d7c77c1543a26d6e7c4776a
Cristine Haber <haber650@gmail.com>
2024-05-23 16:51:04 +0800

    Update Delete Modal
    Truck

--- resources/views/components/truck/delete.blade.php
+++ resources/views/components/truck/delete.blade.php
@@ -5,16 +5,16 @@
             <form method="POST" action="{{ route('admin.trucks.delete', ['id' => $truck->id]) }}">
                 @csrf
                 @method('DELETE')
-                <div class="modal-header">
-                    <h5 class="modal-title" id="deleteTruckModalLabel">Delete Truck</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-body text-center p-0">
+                    <div class="modal-icon mt-3">
+                        <img src="/assets/images/icons/delete.gif" alt="Warning Icon">
+                    </div>
+                    <h4 class="modal-title"id="deleteTruckModalLabel">Delete this <strong>"{{ $truck->name }}"</strong> truck?</h4>
+                    <p>The data associated with this rank will be lost.</p>
                 </div>
-                <div class="modal-body">
-                    <p>Are you sure you want to delete this truck?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-danger">Delete</button>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-danger">Yes, Delete!</button>
                 </div>
             </form>
         </div>
