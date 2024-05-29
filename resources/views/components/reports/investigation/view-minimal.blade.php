b71d4967382f1156a4f535253ec468db94fa297f
Don John Daryl P. Curativo <djohncurativo6161@gmail.com>
2024-05-29 20:05:06 +0800

    errors hayys Fixed

--- resources/views/components/reports/investigation/view-minimal.blade.php
+++ resources/views/components/reports/investigation/view-minimal.blade.php
@@ -2,6 +2,7 @@
     <div class="modal-dialog modal-xl modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header pt-4 px-4 pb-1">
+                {{-- {{dd($investigation)}} --}}
                 <h3 class="modal-title fw-bolder text-primary">{{ $investigation->investigation->subject }}</h3>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
