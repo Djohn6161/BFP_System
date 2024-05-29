053dc82d9cde5a00db8b9c83f25e4445164e47c8
Benedict Reyes <hanzbndctreyes18@gmail.com>
2024-05-29 13:45:52 +0800

    View Investigation Logs Modal

--- resources/views/components/logs/view-investigation.blade.php
+++ resources/views/components/logs/view-investigation.blade.php
@@ -1,17 +1,77 @@
-<div class="modal fade" tabindex="-1" id="viewInvestigationLogs">
-    <div class="modal-dialog modal-xl modal-dialog-scrollable">
+<div class="modal fade" tabindex="-1" id="viewInvestigationLogs{{ $log->id }}">
+    <div class="modal-dialog modal-lg modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header pt-4 px-4 pb-1">
-                <h3 class="modal-title fw-bolder text-primary">HAHAHAHA</h3>
+                <h4 class="modal-title fw-bolder text-primary">Investigation</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <hr>
-            <div>
-                <h5>Date time</h5>
-                <p>{{ $log->updated_at }}</p>
-            </div>
-                <h5>User</h5>
-                <p>{{ $log->user->name }}</p>
+            <div class="modal-body px-4 py-3">
+                <table class="table table-striped">
+                    <tr>
+                        <th>Made Changes</th>
+                        <td class="text-break">
+                            @if ($log->action == 'Delete')
+                            <div class="text-light rounded-pill  bg-danger p-2 text-center" style="width: 70px;">
+                                {{ $log->action }}</div>
+                            @elseif($log->action == 'Update')
+                                <div class="text-light rounded-pill bg-success p-2 text-center" style="width: 70px;">
+                                    {{ $log->action }}</div>
+                            @elseif($log->action == 'Store')
+                                <div class="text-light rounded-pill bg-primary p-2 text-center" style="width: 70px;">
+                                    {{ $log->action }}</div>
+                            @else
+                                <div class="rounded-pill p-2" style="width: 70px;">{{ $log->action }}
+                                </div>
+                            @endif
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Date and Time</th>
+                        <td class="text-break">{{ $log->updated_at }}</td>
+                    </tr>
+                    <tr>
+                        <th>User</th>
+                        <td class="text-break">{{ $log->user->name }}</</td>
+                    </tr>
+                    <tr>
+                        <th>Investigation ID</th>
+                        <td class="text-break">{{$log->investigation->id}} - @if ($log->investigation->spot)
+                            Spot
+                            @elseif($log->investigation->minimal)
+                            Minimal
+                            @elseif($log->investigation->progress)
+                            Progress
+                            @elseif($log->investigation->final)
+                            Final
+                             @endif
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Investigation Date</th>
+                        <td class="text-break">{{ $log->investigation != null ? $log->investigation->date : 'Unavailable' }}</td>
+                    </tr>
+                </table>
+                <hr>
+                <div>
+                    <h6 class="my-4 fw-bolder">Details</h6>
+                    <p>
+                        @if ($log->action == 'Update')
+                        @php
+                            $changes = json_decode($log->details, true);
+                            // dd($changes);
+                        @endphp
+                        @foreach ($changes as $column => $change)
+                            <h6 class="text-capitalize text-primary"><strong>{{ $column }}</strong></h6>
+                            <p>
+                                <b><i>FROM: </i></b> "{{" ". $change['old'] }}" <br> <b><i>TO: </i></b>"{!! $change['new'] !!}"<br>
+                            </p>
+                        @endforeach
+                        @else
+                        {{ $log->details }}
+                        @endif
+                    </p>
+                </div>
             </div>
         </div>
     </div>
