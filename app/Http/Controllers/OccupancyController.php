4893caa5f239216cad70dcb4f1941fc32002017c
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-05-29 16:18:51 +0800

    Occupancy log Added

--- app/Http/Controllers/OccupancyController.php
+++ app/Http/Controllers/OccupancyController.php
@@ -6,8 +6,9 @@ use App\Models\Occupancy;
 
 use Illuminate\Http\Request;
 use App\Models\Occupancy_name;
-use Illuminate\Contracts\Support\ValidatedData;
+use App\Models\ConfigurationLog;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Support\ValidatedData;
 
 class OccupancyController extends Controller
 {
@@ -27,7 +28,16 @@ class OccupancyController extends Controller
             
         ]);
 
-        Occupancy_name::create($validatedData);
+        $occu = Occupancy_name::create($validatedData);
+        $log = new ConfigurationLog();
+
+        $log->fill([
+            'userID' => auth()->user()->id,
+                'Details' => "Created a occupancy with a name of <b>" . $occu->name . "</b>",
+                'type' => 'occupancy',
+                'action' => 'Store',
+        ]);
+        $log->save();
         return redirect()->back()->with('success', 'Occupancy, created Successfully.');
        
     }
@@ -42,7 +52,27 @@ class OccupancyController extends Controller
             
         ]);
             
+        $occoChanges = $this->hasChanges($occupancyName, $validatedData);
+        $string = "Updated Occupancy " . $occupancyName->name;
 
+        if ($occoChanges) {
+            foreach ($occoChanges as $index => $change) {
+                $format = str_replace('_', ' ', $index);
+                $format = ucwords($format);
+
+                $string = $string . "<li>" . "<b>" . $format . "</b>" . ": " . $occupancyName[$index] . " -> " . $change . "</li>";
+            }
+        }
+
+        // dd($formFields, $InfoUpdatedData);
+        $log = new ConfigurationLog();
+        $log->fill([
+            'userID' => auth()->user()->id,
+                'Details' => $string,
+                'type' => 'occupancy',
+                'action' => 'Update',
+        ]);
+        $log->save();
         $occupancyName->update($validatedData);
         return redirect()->back()->with('success', 'Occupancy, update Successfully.');
        
@@ -50,11 +80,28 @@ class OccupancyController extends Controller
     public function deleteOccupancyName($id)
     {
         $occupancyName = Occupancy_Name::findOrFail($id);
-
+        $log = new ConfigurationLog();
+        $log->fill([
+            'userID' => auth()->user()->id,
+                'Details' => "Deleted Designation <b>". $occupancyName->name . "</b>",
+                'type' => 'occupancy',
+                'action' => 'Delete',
+        ]);
+        $log->save();
        $occupancyName->delete();
         // $occupancyName->update($validatedData);
         return redirect()->back()->with('success', 'Occupancy, update Successfully.');
        
     }
+    private function hasChanges($info, $updatedData) {
+        $changes = [];
 
+        foreach ($updatedData as $key => $value) {
+            if ($info->{$key} != $value) {
+                $changes[$key] = $value;
+            }
+        }
+
+        return $changes;
+    }
 }
