e479f8e25379a1525e21b37e3483ed757ca74ed9
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2024-05-30 09:11:31 +0800

    Middleware Prevent Back button

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkPrivilege' => \App\Http\Middleware\checkPrivilege::class,
+        'PreventBack' => \App\Http\Middleware\PreventBack::class,
     ];
 }
