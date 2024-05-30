8b3bd94e832f75c2278cf8c55c513a6be4c89de0
Benedict Reyes <hanzbndctreyes18@gmail.com>
2024-05-30 16:09:49 +0800

    Update index.blade.php

--- resources/views/admin/designation/index.blade.php
+++ resources/views/admin/designation/index.blade.php
@@ -6,7 +6,7 @@
             <ol class="breadcrumb mb-0">
                 <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
                 <li class="breadcrumb-item"><a href="">Configurations</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Designation</li>
+                <li class="breadcrumb-item active" aria-current="page">Designations</li>
             </ol>
         </nav>
         <div class="col-lg-12">
