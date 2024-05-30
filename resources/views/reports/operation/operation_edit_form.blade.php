7a5d24f830fa4c381a86403d1b0b34a055bca286
Craig Ben Cadag <craigbencadag@gmail.com>
2024-05-30 11:11:17 +0800

    Operation Update
    Operation Edit Form
    Operation Form

--- resources/views/reports/operation/operation_edit_form.blade.php
+++ resources/views/reports/operation/operation_edit_form.blade.php
@@ -5,7 +5,7 @@
             <div class="col-lg-11 p-4">
                 <div class="row">
                     <form method="POST" action="{{ route('operation.update') }}" enctype="multipart/form-data">
-                        @csrf 
+                        @csrf
 
                         <div class="row mb-3">
                             <div class="col d-flex justify-content-start px-0">
@@ -17,10 +17,10 @@
                                 </a>
                             </div>
                         </div>
-                        
+
                         <!-- Intro -->
                         <div class="row border border-light-subtle shadow rounded p-4 mb-4 bg-white">
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">1</h3>
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Operation Information</h3>
                             <div class="col-lg-6 mb-3">
                                 <label for="alarmReceived" class="form-label">Alarm Received
                                     (Time)</label>
@@ -92,7 +92,7 @@
                         <div class="row border border-light-subtle shadow rounded p-4 mb-4 bg-white">
                             <div class="row m-0 p-0" id="divApor">
                                 <div class="row m-0 p-0 border-0" id="addApor">
-                                    <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">2
+                                    <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Engine Dispatched
                                     </h3>
                                     @foreach ($responses as $response)
                                         <div class="row remove-button-container m-0 p-0">
@@ -180,8 +180,9 @@
 
                         <!-- Alarm -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">3 and
-                                9</h3>
+                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">3 and
+                                9</h3> --}}
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Alarm Declared</h3>
                             <div class="col-lg-6">
                                 <label for="alarmStatus" class="form-label">Alarm
                                     Status</label>
@@ -207,16 +208,14 @@
                             <div class="col-lg-6 mb-3">
                                 <label for="underControl" class="form-label">Time / Date Under
                                     Control</label>
-                                <input type="datetime-local" class="form-control"
-                                    id="firstResponderInput" name="td_under_control"
-                                    value="{{ $operation->td_under_control }}">
+                                <input type="datetime-local" class="form-control" id="firstResponderInput"
+                                    name="td_under_control" value="{{ $operation->td_under_control }}">
                             </div>
                             <div class="col-lg-6 mb-3">
                                 <label for="fireOut" class="form-label">Time / Date Declared
                                     Fire Out</label>
-                                <input type="datetime-local" class="form-control"
-                                    id="firstResponderInput" name="td_declared_fireout"
-                                    value="{{ $operation->td_under_control }}">
+                                <input type="datetime-local" class="form-control" id="firstResponderInput"
+                                    name="td_declared_fireout" value="{{ $operation->td_under_control }}">
                             </div>
                             <hr>
                             <div class="row m-0 p-0" id="secondDivApor">
@@ -284,7 +283,7 @@
 
                         <!-- Occupancy -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">4-6
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Occupancies
                             </h3>
                             <div class="col-lg-4 mb-3">
                                 <label for="typeOfOccupancy" class="form-label">Occupancy name</label>
@@ -334,7 +333,7 @@
 
                         <!-- Casualties -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Total Number of Casualty Reported</h3> --}}
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Total Number of Casualty Reported</h3>
                             <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">7
                             </h3>
 
@@ -537,7 +536,8 @@
 
                         <!-- Duty Personnel -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Duty Personnel at the Fire Scene</h3>
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Duty Personnel at the Fire Scene
+                            </h3>
                             <div class="row m-0 p-0" id="thirdDivApor">
                                 <div class="row m-0 p-0" id="thirdAddApor">
                                     @foreach ($operation->dutyPersonnels as $dutyPersonnel)
@@ -599,7 +599,7 @@
 
                         <!-- Photos -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">14</h3>
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Duty Personnel at the Fire Scene</h3>
                             <label class="form-label" for="exampleCheck1">Photos</label>
                             <input type="file" class="form-control uncheable" id="photos"
                                 name="sketch_of_fire_operation[]" multiple>
@@ -625,9 +625,9 @@
 
                         <!-- Details narrative -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Details (Narrative)</h3> --}}
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">15
-                            </h3>
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Details</h3>
+                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">15
+                            </h3> --}}
                             <div class="col-lg-12 mb-3">
                                 <label for="firefighterDeath" class="form-label">Details
                                     (Narrative)</label>
@@ -637,9 +637,9 @@
 
                         <!-- Problem encounterd -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Problem/s Encountered During Operation</h3> --}}
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">16
-                            </h3>
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Problem/s Encountered During Operation</h3>
+                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">16
+                            </h3> --}}
                             <div class="col-lg-12 mb-3">
                                 <label for="firefighterDeath" class="form-label">Problems /
                                     Encountered during operation:</label>
@@ -649,9 +649,9 @@
 
                         <!-- Observation Recommendation -->
                         <div class="row border border-light-subtle shadow rounded my-3 p-4 bg-white">
-                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Observations/Recommendations</h3> --}}
-                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">17
-                            </h3>
+                            <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">Observations/Recommendations</h3>
+                            {{-- <h3 class="border-bottom border-4 border-secondary pb-2 mb-3">17
+                            </h3> --}}
                             <div class="col-lg-12 mb-3">
                                 <label for="firefighterDeath" class="form-label">Observation /
                                     Recommendation</label>
