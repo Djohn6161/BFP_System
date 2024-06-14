7e30f06280d94661629cf28ceb5619b09160aa4f
Joshua Abinal <xantinou23@gmail.com>
2024-06-14 16:31:00 +0800

    Update edit_password.blade.php
    Create show and hide password

--- resources/views/components/account/edit_password.blade.php
+++ resources/views/components/account/edit_password.blade.php
@@ -15,25 +15,29 @@
             <form action="{{ route('admin.account.password.update') }}" method="POST">
                 @csrf
                 <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Current Password:</label>
+                    <label for="current_password" class="form-label">Current Password:</label>
+                    <div class="input-group show-password mb-3" id="show_hide_password_current">
+                        <input type="password" class="form-control" name="current_password" id="current_password" placeholder="Enter Current Password">
                         <input type="hidden" name="password_id" id="update_password_id">
-                        <input type="password" class="form-control" name="current_password"
-                            placeholder="Enter Current Password">
+                        <span class="input-group-text"><a href="#"><i class="ti ti-eye-off" aria-hidden="true"></i></a></span>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">New Password:</label>
+            
+                    <label for="password" class="form-label">New Password:</label>
+                    <div class="input-group show-password mb-3" id="show_hide_password_new">
                         <input type="password" class="form-control" name="password" placeholder="Enter Password">
+                        <span class="input-group-text"><a href="#"><i class="ti ti-eye-off" aria-hidden="true"></i></a></span>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Confirm Password:</label>
-                        <input type="password" class="form-control" placeholder="Confirm Password"
-                            name="confirm_password">
+                    
+                    <label for="confirm_password" class="form-label">Confirm Password:</label>
+                    <div class="input-group show-password mb-3" id="show_hide_password_confirm">
+                        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password">
+                        <span class="input-group-text"><a href="#"><i class="ti ti-eye-off" aria-hidden="true"></i></a></span>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Admin Confirmation Password:</label>
-                        <input type="password" class="form-control"
-                            placeholder="Admin Confirm Password" name="admin_confirm_password">
+                    
+                    <label for="admin_confirm_password" class="form-label">Admin Confirmation Password:</label>
+                    <div class="input-group show-password mb-3" id="show_hide_password_admin">
+                        <input type="password" class="form-control" name="admin_confirm_password" placeholder="Admin Confirm Password">
+                        <span class="input-group-text"><a href="#"><i class="ti ti-eye-off" aria-hidden="true"></i></a></span>
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -50,8 +54,24 @@
         $('#updatePasswordModal').on('show.bs.modal', function(event) {
             var button = $(event.relatedTarget);
             var accountId = button.data('account-id');
- 
+
             $('#update_password_id').val(accountId);
+        });
+
+        $(".show-password a").on('click', function(event) {
+            event.preventDefault();
+            var passwordField = $(this).closest('.show-password').find('input[type="password"], input[type="text"]');
+            var icon = $(this).find('i');
+            
+            if (passwordField.attr("type") == "text") {
+                passwordField.attr('type', 'password');
+                icon.addClass("ti-eye-off");
+                icon.removeClass("ti-eye");
+            } else if (passwordField.attr("type") == "password") {
+                passwordField.attr('type', 'text');
+                icon.removeClass("ti-eye-off");
+                icon.addClass("ti-eye");
+            }
         });
     });
 </script>
